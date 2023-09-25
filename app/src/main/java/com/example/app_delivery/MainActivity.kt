26edc873e50cb803785c356014a1beb6acb67055
xx-sun-xx <xxsunxxmoon@gmail.com>
2023-09-25 02:16:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/app_delivery/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app_delivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.comeco_main)
+    }
+}
